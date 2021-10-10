58596c918d1917600ab730aae4126c19a078a1e3
camiloJTG <camilojtg.dev@protonmail.com>
2021-10-10 12:10:28 -0300

    feat: Add new url base in rest countries api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://restcountries.eu/rest/v2',
+  baseUrl: 'https://restcountries/rest/v2',
   cantPerPage: 20,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://restcountries.eu/rest/v2',
+  baseUrl: 'https://restcountries/rest/v2',
   cantPerPage: 20,
 };
 
